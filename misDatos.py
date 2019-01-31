ac23581e59689920b3a91452dc503147ef939cdb
josemacvz <46934787+josemacvz@users.noreply.github.com>
2019-01-30 21:10:52 -0600

    Update misDatos.py

--- misDatos.py
+++ misDatos.py
@@ -1,4 +1,21 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: José uis Macías Vázquez, A01655713
+# Descripcion: Recopilará los datos del aumno y los entregará juntos.
 
 # Escribe tu programa después de esta línea.
+
+nombre = input("Escribe tu nombre combre completo: ")
+matricula = input("Escribe tu matricula: ")
+carrera1 = input("Escribe tu carrera: ")
+prepa = input("Escribe el nombre tu escuela de procedencia: ")
+escribe = input("Describete en dos o tres líneas tus pasatiempos, habilidades, deportes, libros preferidos, viajes, pasateimpos, etc: ")
+
+print("Nombre: ", nombre)
+
+print("Matricula: ", matricula)
+
+print("Carrera: ", carrera1)
+
+print("Prepa: ", prepa)
+
+print("Descripción: ", escribe)
+
