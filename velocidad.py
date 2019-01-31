d74563e4108c7ed11475bd44feb8a4bf67e77972
josemacvz <46934787+josemacvz@users.noreply.github.com>
2019-01-30 21:14:00 -0600

    Update velocidad.py

--- velocidad.py
+++ velocidad.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: José Luis Macías Vázquez, A01655713, Grupo 03
+# Calcula velocidad promedio, dado el tiempo y la distancia
 
-# Escribe tu programa después de esta línea.
+velocidad = int(input("Escriba la velocidad a la que viaja el carro en número entero: "))
+
+distanciaUno = velocidad*6
+
+distanciaDos = velocidad*3.5
+
+tiempoHorasM = 485/velocidad
+
+print("Distancia recorrida en 6 hrs: ", distanciaUno, "km")
+print("Distancia recorrida en 3.5 hrs: ", distanciaDos, "km")
+print("Tiempo para recorrer 485 km: ", tiempoHorasM, "hrs")
+
