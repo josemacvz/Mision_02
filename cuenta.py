b1d9f29bd3dcf1d24c5c032af2ae42483564091b
josemacvz <46934787+josemacvz@users.noreply.github.com>
2019-01-30 21:15:30 -0600

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: José Luis Macías Vázquez, A01655713, Grupo 03
+# Descripcion: Entrega la cuenta más propina e IVA y el desgloce de cada cargo.
 
 # Escribe tu programa después de esta línea.
+
+subtotal = int(input("Digite el total de la cuenta: "))
+propina = subtotal*1.13
+iva = subtotal*1.16
+totalAPagar = subtotal+(propina-subtotal)+(iva-subtotal)
+
+print("Costo de su comida: $",subtotal)
+print("Propina: $",propina-subtotal)
+print("IVA: $",iva-subtotal)
+print("Total a pagar: $",totalAPagar)
+
+
